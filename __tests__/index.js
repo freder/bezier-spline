eb4cadd1d2ce1e44665f31bc7cea1fc6c3e9dba8
Frederic Brodbeck <freder.code@gmail.com>
2017-03-14 17:09:23 +0100

    alternatingly merge two lists

--- __tests__/index.js
+++ __tests__/index.js
@@ -14,3 +14,23 @@ test('transpose()', () => {
 	expect(result[1]).toEqual(['x2', 'y2', 'z2']);
 	expect(result[2]).toEqual(['x3', 'y3', 'z3']);
 });
+
+
+test('alternate()', () => {
+	const a = ['a1', 'a2', 'a3'];
+	const b = ['b1', 'b2'];
+	const result = bezierSpline.alternate(a, b);
+	expect(result).toEqual(['a1', 'b1', 'a2', 'b2', 'a3']);
+});
+test('alternate()', () => {
+	const a = ['a1', 'a2', 'a3'];
+	const b = ['b1', 'b2', 'b3'];
+	const result = bezierSpline.alternate(a, b);
+	expect(result).toEqual(['a1', 'b1', 'a2', 'b2', 'a3', 'b3']);
+});
+test('alternate()', () => {
+	const a = ['a1', 'a2', 'a3'];
+	const b = ['b1', 'b2', 'b3', 'b4', 'b5'];
+	const result = bezierSpline.alternate(a, b);
+	expect(result).toEqual(['a1', 'b1', 'a2', 'b2', 'a3', 'b3', undefined, 'b4', undefined, 'b5']);
+});
--- src/index.js
+++ src/index.js
@@ -17,3 +17,17 @@ function transpose(lists) {
 			[]
 		);
 };
+
+
+const alternate = 
+module.exports.alternate = 
+function alternate(a, b) {
+	return b
+		.reduce(
+			(acc, item, i) => {
+				acc[1 + (i * 2)] = item;
+				return acc;
+			},
+			R.intersperse(undefined, a)
+		)
+};
