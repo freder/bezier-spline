7a485e8ddc25a9bbf27deb4d7a330aa8d6e83f96
Frederic Brodbeck <freder.code@gmail.com>
2017-03-14 17:25:16 +0100

    clean up

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,6 @@
 /* global test: true */
 /* global expect: true */
-const bezierSpline = require('../src/index.js');
+const transpose = require('../src/transpose.js');
 
 
 test('transpose()', () => {
@@ -9,28 +9,8 @@ test('transpose()', () => {
 		['y1', 'y2', 'y3'],
 		['z1', 'z2', 'z3']
 	];
-	const result = bezierSpline.transpose(lists);
+	const result = transpose(lists);
 	expect(result[0]).toEqual(['x1', 'y1', 'z1']);
 	expect(result[1]).toEqual(['x2', 'y2', 'z2']);
 	expect(result[2]).toEqual(['x3', 'y3', 'z3']);
-});
-
-
-test('alternate()', () => {
-	const a = ['a1', 'a2', 'a3'];
-	const b = ['b1', 'b2'];
-	const result = bezierSpline.alternate(a, b);
-	expect(result).toEqual(['a1', 'b1', 'a2', 'b2', 'a3']);
-});
-test('alternate()', () => {
-	const a = ['a1', 'a2', 'a3'];
-	const b = ['b1', 'b2', 'b3'];
-	const result = bezierSpline.alternate(a, b);
-	expect(result).toEqual(['a1', 'b1', 'a2', 'b2', 'a3', 'b3']);
-});
-test('alternate()', () => {
-	const a = ['a1', 'a2', 'a3'];
-	const b = ['b1', 'b2', 'b3', 'b4', 'b5'];
-	const result = bezierSpline.alternate(a, b);
-	expect(result).toEqual(['a1', 'b1', 'a2', 'b2', 'a3', 'b3', undefined, 'b4', undefined, 'b5']);
 });
--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
 const R = require('ramda');
 const computeControlPoints = require('./computeControlPoints.js');
+const transpose = require('./transpose.js');
 
 
-const transpose = 
-module.exports.transpose = 
-function transpose(lists) {
-	const len = Math.min(
-		...(lists.map(R.prop('length')))
+const controlPointsToLists = (controlPoints) => {
+	return R.flatten(
+		transpose([
+			controlPoints.p1,
+			controlPoints.p2,
+		])
 	);
-	return R.range(0, len)
-		.reduce(
-			(acc, i) => [
-				...acc, 
-				lists.map(R.nth(i))
-			], 
-			[]
-		);
 };
 
 
-const alternate = 
-module.exports.alternate = 
-function alternate(a, b) {
-	return b
-		.reduce(
-			(acc, item, i) => {
-				acc[1 + (i * 2)] = item;
-				return acc;
-			},
-			R.intersperse(undefined, a)
-		)
-};
